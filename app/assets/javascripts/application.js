b9604a8214e4ce7698459d96cca71282e21efa46
hungmi <hungmihungmi@gmail.com>
2015-07-08 17:16:16 +0800

    加入 已點餐點欄位 以及 剛加入餐點的欄位
    TODO：
      點餐前可選擇數量
      點餐表格搜尋修復
    
    OPTIMIZE：
      使用同一個fooditem_field for orders#new and #edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,25 +27,34 @@ $(function() {
     $('#you_can_order_now').removeClass('hide');
   });
 
+
   $('.fooditem_quantity').on('change', function() {
     console.log('aaa');
-
   });
 
-  $('#add_fooditem_btn').click(function(){
-    console.log('aaa');
-    $(this).text("繼續加入");
+  $('.add_fooditem_btn').click(function(){
+    console.log('aaabc');
+    $(this).text("已加入");
+    $(this).addClass('disabled');
   });
 
   $('#food_table').DataTable({
     ordering: false,
-    paging: false,
+    paging: true,
+    //"pageLength": 4,
     info: false,
   });
 
+  // 換頁之後對新的一頁做連結初始化
+  $('#food_table').on('draw.dt', function() {
+    console.log('draw');
+    $("#food_table .links a.add_fields").
+      data("association-insertion-method", 'append').
+      data("association-insertion-node", '#your_foods');  
+  });
 
-  $(".links a.add_fields").
+  $("#food_table .links a.add_fields").
     data("association-insertion-method", 'append').
-    data("association-insertion-node", '#your_foods');
+    data("association-insertion-node", '#your_foods');  
   
 });
